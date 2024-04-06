07edc760bb7705fbeb1baf066c647cf54d5a7a69
Mohamed Omar Moussa <mohamedomarmoussa02@gmail.com>
2024-04-06 17:38:36 +0200

    clerk_auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import MobileNav from './MobileNav'
+import { SignedIn } from '@clerk/nextjs'
+import { UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
   return (
@@ -18,9 +20,11 @@ const Navbar = () => {
           className='max-sm:size-10'
         />
         <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Moom</p>
-      </Link> 
+      </Link>
       <div className='flex-between gap-5'>
-        {/* Clerk - User Management */}
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
         <MobileNav />
       </div>
     </nav>
