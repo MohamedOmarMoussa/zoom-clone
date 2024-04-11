07326558ecdae80a8db5a4ba0209569b865150d8
Mohamed Omar Moussa <mohamedomarmoussa02@gmail.com>
2024-04-12 00:43:31 +0200

    meeting-room_completed

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,6 +1,15 @@
 import Navbar from '@/components/Navbar'
 import Sidebar from '@/components/Sidebar'
+import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: "Moom",
+  description: "Video calling app",
+  icons: {
+    icon: "/icons/logo.svg"
+  }
+};
 
 const HomeLayout = ({ children }: { children: ReactNode }) => {
   return (
