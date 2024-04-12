43329ad90e9644cb195f89cd9e99036947f48e6b
Mohamed Omar Moussa <mohamedomarmoussa02@gmail.com>
2024-04-12 23:47:59 +0200

    join_meeting_modal_done

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,73 +1,89 @@
-import { cn } from '@/lib/utils'
-import { CallControls, CallParticipantsList, CallStatsButton, CallingState, PaginatedGridLayout, SpeakerLayout, useCallStateHooks } from '@stream-io/video-react-sdk'
-import React, { useState } from 'react'
+'use client';
+import { useState } from 'react';
+import {
+  CallControls,
+  CallParticipantsList,
+  CallStatsButton,
+  CallingState,
+  PaginatedGridLayout,
+  SpeakerLayout,
+  useCallStateHooks,
+} from '@stream-io/video-react-sdk';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { Users, LayoutList } from 'lucide-react';
+
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { LayoutList, Users } from 'lucide-react'
-import { useSearchParams } from 'next/navigation'
-import EndCallButton from './EndCallButton'
-import Loader from './Loader'
+} from './ui/dropdown-menu';
+import Loader from './Loader';
+import EndCallButton from './EndCallButton';
+import { cn } from '@/lib/utils';
 
-
-type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right'
+type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right';
 
 const MeetingRoom = () => {
   const searchParams = useSearchParams();
-  const isPersonalRoom = !!searchParams.get('personal')
-  const [layout, setLayout] = useState<CallLayoutType>('speaker-left')
-  const [showParticipants, setShowParticipants] = useState(false)
+  const isPersonalRoom = !!searchParams.get('personal');
+  const router = useRouter();
+  const [layout, setLayout] = useState<CallLayoutType>('speaker-left');
+  const [showParticipants, setShowParticipants] = useState(false);
+  const { useCallCallingState } = useCallStateHooks();
 
-  const { useCallCallingState} = useCallStateHooks();
+  // for more detail about types of CallingState see: https://getstream.io/video/docs/react/ui-cookbook/ringing-call/#incoming-call-panel
   const callingState = useCallCallingState();
 
-  if(callingState !== CallingState.JOINED) return <Loader />
+  if (callingState !== CallingState.JOINED) return <Loader />;
 
   const CallLayout = () => {
     switch (layout) {
       case 'grid':
-        return <PaginatedGridLayout />
+        return <PaginatedGridLayout />;
       case 'speaker-right':
-        return <SpeakerLayout participantsBarPosition='left' />
+        return <SpeakerLayout participantsBarPosition="left" />;
       default:
-        return <SpeakerLayout participantsBarPosition='right' />
+        return <SpeakerLayout participantsBarPosition="right" />;
     }
-  }
+  };
 
   return (
-    <section className='relative h-screen w-full overflow-hidden pt-4 text-white'>
-      <div className='relative flex size-full items-center justify-center'>
-        <div className='flex size-full max-w-[1000px] items-center'>
+    <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
+      <div className="relative flex size-full items-center justify-center">
+        <div className=" flex size-full max-w-[1000px] items-center">
           <CallLayout />
         </div>
-        <div className={cn('h-[calc(100vh-86px)] hidden ml-2', { 'show-block': showParticipants})}>
+        <div
+          className={cn('h-[calc(100vh-86px)] hidden ml-2', {
+            'show-block': showParticipants,
+          })}
+        >
           <CallParticipantsList onClose={() => setShowParticipants(false)} />
         </div>
       </div>
-      <div className='fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap'>
-        <CallControls />
+      {/* video layout and call controls */}
+      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+        <CallControls onLeave={() => router.push(`/`)} />
+
         <DropdownMenu>
           <div className="flex items-center">
             <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
               <LayoutList size={20} className="text-white" />
             </DropdownMenuTrigger>
           </div>
-          <DropdownMenuContent className='border-dark-1 bg-dark-1 text-white'>
+          <DropdownMenuContent className="border-dark-1 bg-dark-1 text-white">
             {['Grid', 'Speaker-Left', 'Speaker-Right'].map((item, index) => (
               <div key={index}>
                 <DropdownMenuItem
-                  className='cursor-pointer'
-                  onClick={() => {
-                    setLayout(item.toLocaleLowerCase() as CallLayoutType)
-                  }}
+                  onClick={() =>
+                    setLayout(item.toLowerCase() as CallLayoutType)
+                  }
                 >
                   {item}
                 </DropdownMenuItem>
-                <DropdownMenuSeparator className='border-dark-1' />
+                <DropdownMenuSeparator className="border-dark-1" />
               </div>
             ))}
           </DropdownMenuContent>
@@ -81,7 +97,7 @@ const MeetingRoom = () => {
         {!isPersonalRoom && <EndCallButton />}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default MeetingRoom
+export default MeetingRoom;
